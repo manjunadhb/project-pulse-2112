05159204e79a37faf3a60f16530e055cffb606f8
Venkatesh Pathakota <65524430+venkateshp153@users.noreply.github.com>
2022-04-28 16:34:18 +0530

    modified

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,10 +1,37 @@
 import AdminHeader from "./AdminHeader"
+ 
+import { Dropdown, Nav } from "react-bootstrap"
+import DropdownItem from "react-bootstrap/esm/DropdownItem"
 
 const Ranking=()=>{
+   
     return(
         <>
         <AdminHeader/>
-        <h3>Ranking</h3>
+        <ul className="col-sm-12 mx-2 my-4">
+            <li><p>Report For</p></li>
+            <li><select >
+               <option> <div className="ui search">
+                    <div className="ui icon input">
+                        <input type='text' placeholder=" Search" className="prompt"></input>
+                        <i className="search icon"></i>
+                    </div>
+                </div>
+                </option>
+                <option>2114</option>
+                <option>2113</option>
+                <option>2112</option>
+                <option>2111</option>
+                <option>2110</option>
+                <option>2109</option>
+                <option>2108</option>
+                <option disabled>Completed Batches</option>
+                </select>
+            </li>
+            <li><button type="button" className="btn btn-primary">Get Report</button></li>
+        </ul>
+        
+
         </>
 
     )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,263 @@
 import Header from "./Header";
-
+import Table from 'react-bootstrap/Table'
 const Tasks = () => {
   return (
     <>
       <Header />
-      <div class="alert alert-danger" role="alert">
-        <h1>Tasks</h1>
+      <div class="alert" role="alert">
+        <spam style={{fontSize:'2rem'}}>Tasks</spam><spam>Tasks assigned to you for completion and upload</spam>
+        
+        <div className="container-12 mx-2 my-2">
+          <div className="card">
+          <div className="card-header" style={{backgroundColor:'rgba(92, 176, 230, 0.99)',color:'white'}}>💬Tasks Assigned to You</div>
+          <div className="row">
+           
+          <select className="col-1 my-4 mx-4" style={{padding:'8px'}}>
+            <option>5</option>
+            <option>15</option>
+            <option>20</option>
+            <option selected>All</option>
+            
+          </select>
+          
+          </div>
+          <table>
+            <th>
+              <thead>
+              <td>#</td>
+              <td>UID</td>
+              <td>ID</td> 
+              <td>Type</td>
+              <td>Title</td>
+              <td>Requirements</td>
+              <td>issues</td>
+              <td>Build to Store</td>
+              <td>Deadline</td>
+              <td>Max.File Size</td>
+              <td>Status</td>
+              <td>Points Earned</td>
+              </thead>
+            </th>
+            <tr>
+              <tbody className="success">
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>2</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>3</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>4</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+              </tbody>
+            </tr>
+            <tr>
+              <tbody>
+                <td>1</td>
+                <td>BTT1</td>
+                <td>46457</td>
+                <td>Technical Task-Theory()</td>
+                <td>Variables and Datatypes</td>
+                <td>Please go thru variables, datatypes and its memory allocations and ranges.</td>
+                <td>Open : 0
+                Fixed : 0
+                Closed : 0</td>
+                <td>NA</td>
+                <td>Before 11:00 am on 28 January 2021</td>
+                <td>Not more than 0 MB</td>
+                <td><a href='#'>Review Pending  </a></td>
+                <td>0/100</td>
+                
+              </tbody>
+            </tr>
+          </table>
+          </div>
+        </div>
       </div>
     </>
   );
