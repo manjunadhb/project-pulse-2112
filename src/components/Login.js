dba9d465aa734d72fc61da3050236d6cada8c319
arif <mirzaarifali3@gmail.com>
2022-04-28 11:33:49 +0530

    login edited

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,33 +3,63 @@ import { Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  
-const [loggedIn,setLoggedIn] = useState(true)
-let navigation=useNavigate();
- const  studentHandler = (e)=>{
-             e.preventDefault();
-             if(loggedIn){
-                 navigation("/home")
-             }
-}
- const  adminHandler = (e)=>{
-             e.preventDefault();
-             if(loggedIn){
-                 navigation("/Admin")
-             }
-}
+  const student = {
+    email: "student210101@gmail.com",
+    password: "abc",
+  };
+  const admin = {
+    email: "pulseadmin@gmail.com",
+    password: "pulseadmin",
+  };
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loggedIn, setLoggedIn] = useState(true);
+  let navigation = useNavigate();
+  const studentHandler = (e) => {
+    e.preventDefault();
 
-const changeHandler =(e)=>{
-      console.log(e.target.value)
-      
-}
+    
+      if (loggedIn) {
+        navigation("/home");
+      }
+    
+  };
+  const adminHandler = (e) => {
+    e.preventDefault();
+
+    if (loggedIn) {
+      navigation("/Admin");
+    }
+  };
+
+  const changeHandler = (e) => {
+    // console.log(e.target.value)
+    const name = e.target.name;
+
+    const value = e.target.value;
+    console.log(value);
+    setData({ ...data, [name]: value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setLoggedIn(true);
+  };
   return (
     <div className="col-4 offset-4 " style={{ marginTop: "12%" }}>
       <h1>Login</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" autoFocus onChange={changeHandler} />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={data.email}
+            autoFocus
+            onChange={changeHandler}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -37,20 +67,21 @@ const changeHandler =(e)=>{
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password"  onChange={changeHandler}/>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+          />
         </Form.Group>
 
-        <Button
-          
-          variant="primary"
-          type="submit"
-          onClick={studentHandler}
-        >
+        <Button variant="primary" type="submit" onClick={studentHandler}>
           Student
         </Button>
 
         <Button
-           className="mx-2"
+          className="mx-2"
           variant="primary"
           type="submit"
           onClick={adminHandler}
