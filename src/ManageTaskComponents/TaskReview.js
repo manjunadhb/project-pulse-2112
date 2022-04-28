bc86d673e9ec7590e383fcd3c48788ce70505723
Tarveen <shaiktarveen007@gmail.com>
2022-04-28 16:12:08 +0530

    deleted taskreview

--- src/ManageTaskComponents/TaskReview.js
+++ src/ManageTaskComponents/TaskReview.js
@@ -3,52 +3,25 @@ import React from 'react'
 function TaskReview() {
   return (
     <div>
-        <table>
+       
             
-           <h6 id="table1">2112 Batch  1.20|15|75 2.15|0|0 3.15|0|0 4.15|0|0</h6>
-         <h6 id="table2">Time Up and 50 sessions left</h6>
+         
         
             <table>
             <tr>
-                <th>Type</th>
-                <th>Allocations</th>
-                <th>Consumed</th>
-                <th>Overall</th>
-                <th>Overall %</th>
+                <th>Task No</th>
+                <th>Task Type</th>
+                <th>Task Title</th>
+                <th>Submission Details</th>
+                <th>Approved</th>
+                <th>Rejected</th>
+                <th>Review Pending</th>
+                <th>Assigned Time</th>
+                <th>Deadline</th>
             </tr>
-            <tr>
-                <td>Class Lab</td>
-                <td>0</td>
-                <td>0</td>
-                <td>15</td>
-                <td>23%</td>
-            </tr>
-
-            <tr>
-                <td>Only Lab</td>
-                <td>0</td>
-                <td>0</td>
-                <td>83</td>
-                <td>237%</td>
-            </tr>
-
-            <tr>
-                <td>Holidays</td>
-                <td>0</td>
-                <td>0</td>
-                <td>24</td>
-                <td>120%</td>
-            </tr>
-
-            <tr>
-                <td>Total Holidays</td>
-                <td>0</td>
-                <td>2</td>
-                <td>122</td>
-                <td>102%</td>
-            </tr>
+            
             </table>
-            </table>  
+            
        
     </div>
   )
